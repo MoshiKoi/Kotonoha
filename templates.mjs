3501f359532133aed952ab58435560f025049dec
Moshi <54333972+MoshiKoi@users.noreply.github.com>
2023-07-13 18:13:43 -0700

    Stylistic improvements

--- templates.mjs
+++ templates.mjs
@@ -5,22 +5,25 @@
 export function createEntry(forms, subentries) {
     console.log(forms, subentries);
     const entryEl = document.createElement('div');
+    entryEl.classList.add('entry')
 
     const headingEl = document.createElement('h3');
     headingEl.innerText = forms.join('・');
 
-    let part_of_speech = null; 
+    let part_of_speech = null;
     const subentryEls = []
-    
+
     for (const subentry of subentries) {
         if (subentry.part_of_speech !== part_of_speech) {
             part_of_speech = subentry.part_of_speech;
             const headEl = document.createElement('h4');
-            headEl.innerText = part_of_speech
+            headEl.classList.add('part-of-speech')
+            headEl.innerText = part_of_speech;
             subentryEls.push(headEl);
         }
 
-        const subentryEl  = document.createElement('ul');
+        const subentryEl = document.createElement('ul');
+        subentryEl.classList.add('gloss-list');
         for (const gloss of subentry.glosses) {
             const glossEl = document.createElement('li');
             glossEl.innerText = gloss.content;
